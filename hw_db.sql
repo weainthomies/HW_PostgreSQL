a8869f85a01524895c3717a3520e4e750d538c2b
weainthomies <weainthomiess@gmail.com>
2025-11-11 15:20:43 +0300

    init commit

---
+++ hw_db.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS Albums (
+    album_id INT SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    year INT
+);
+
+CREATE TABLE IF NOT EXISTS Performers (
+    performer_id INT SERIAL PRIMARY KEY,
+    pseudonym VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Genres (
+    genre_id INT SERIAL PRIMARY KEY,
+    pseudonym VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Collections (
+    collection_id INT SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    year INT
+);
+
+CREATE TABLE IF NOT EXISTS Tracks (
+     track_id INT SERIAL PRIMARY KEY,
+     name VARCHAR(255),
+     duration TIME,
+     album_id INT,
+     FOREIGN KEY (album_id) REFERENCES Albums (album_id)
+);
+
+CREATE TABLE IF NOT EXISTS CollectionsTracks (
+     track_id INT NOT NULL,
+     collection_id INT NOT NULL,
+     PRIMARY KEY (track_id, collection_id),
+     FOREIGN KEY (track_id) REFERENCES Tracks (track_id) ON DELETE CASCADE,
+     FOREIGN KEY (collection_id) REFERENCES Collections (collection_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS PerformersAlbums (
+     performer_id INT NOT NULL,
+     album_id INT NOT NULL,
+     PRIMARY KEY (performer_id, album_id),
+     FOREIGN KEY (performer_id) REFERENCES Performers (performer_id) ON DELETE CASCADE,
+     FOREIGN KEY (album_id) REFERENCES Albums (album_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS PerformersGenres (
+     genre_id INT NOT NULL,
+     performer_id INT NOT NULL,
+     PRIMARY KEY (genre_id, performer_id),
+     FOREIGN KEY (genre_id) REFERENCES Genres (genre_id) ON DELETE CASCADE,
+     FOREIGN KEY (performer_id) REFERENCES Performers (performer_id) ON DELETE CASCADE
+);
